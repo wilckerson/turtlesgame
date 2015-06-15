f3d25b74e357677377a7065b6228f7027ed43943
wilckerson <wilckerson@gmail.com>
2015-06-15 19:47:51 -0300

    movimentacao basica

--- Assets/Scripts/TurtleMovement.cs
+++ Assets/Scripts/TurtleMovement.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class TurtleMovement : MonoBehaviour {
 
+	public float MinPosY = -3;
+	public float MaxPosY = 3;
+	public float MinPosX = -2;
+	public float MaxPosX = 2;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +16,14 @@ public class TurtleMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+
+		var mx = Mathf.Clamp (Input.mousePosition.x / Screen.width, 0, 1);
+		var my = Mathf.Clamp (Input.mousePosition.y / Screen.height, 0, 1);
+		
+		var px = MinPosX + (2 * MaxPosX * mx);
+		var py = MinPosY + (2 * MaxPosY * my); 
+		
+		transform.localPosition = new Vector3 (px, py, transform.localPosition.z);
+
 	}
 }
