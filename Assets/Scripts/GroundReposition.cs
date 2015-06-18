9a72f77ad7daa5fc709b5bd17985266657553e51
wilckerson_pro@hotmail.com <wilckerson_pro@hotmail.com>
2015-06-18 01:25:22 -0300

    Movendo o cenario e criando ilusao de terreno infinito

---
+++ Assets/Scripts/GroundReposition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundReposition : MonoBehaviour {
+
+	public float MinZ = -120f;
+	public float NewZ = 89.5f;
+	public bool ApplyRotation = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (transform.position.z <= MinZ) {
+			transform.position = new Vector3(transform.position.x,transform.position.y,NewZ);
+
+			if(ApplyRotation)
+			{
+				int sign = (Random.value > 0.5f) ? 1 : -1;
+				transform.eulerAngles += new Vector3(0,sign * 90,0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/LevelMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelMovement : MonoBehaviour {
+
+	public float Velocity = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position += new Vector3 (0, 0, -Velocity);
+	}
+}
